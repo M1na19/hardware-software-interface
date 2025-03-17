9c24a8d982910b35f3c81c848b044e85c9aab363
Boanta Mihai <mihaiboanta@yahoo.com>
2025-03-17 19:32:13 +0200

    Lab3

--- labs/lab-03/tasks/binary_search/support/binary_search.c
+++ labs/lab-03/tasks/binary_search/support/binary_search.c
@@ -8,10 +8,24 @@ int binary_search(int *v, int len, int dest)
 {
 	int start = 0;
 	int end = len - 1;
-	int middle;
-
-	/**
-	 * TODO: Implement binary search
-	 */
+bsearch:
+	if (start > end)
+		goto exit;
+	int middle = (start + end) / 2;
+	if (v[middle] == dest)
+		goto found;
+	if (v[middle] > dest)
+		goto to_left;
+	if (v[middle] <= dest)
+		goto to_right;
+to_left:
+	end = middle - 1;
+	goto bsearch;
+to_right:
+	start = middle + 1;
+	goto bsearch;
+found:
+	return middle;
+exit:
 	return -1;
 }
