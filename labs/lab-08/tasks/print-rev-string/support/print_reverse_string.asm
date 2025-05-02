56001b4dd3ba8c678550aee9608fa9bb9903677c
Mihai Boanta <mihaiboanta@yahoo.com>
2025-05-02 14:29:29 +0300

    Lab8

--- labs/lab-08/tasks/print-rev-string/support/print_reverse_string.asm
+++ labs/lab-08/tasks/print-rev-string/support/print_reverse_string.asm
@@ -1,17 +1,51 @@
+section .bss
+	store_string resb 64
 section .text
 extern printf
 extern puts
 global print_reverse_string
 
-; TODO: add the reverse_string() function
-
-print_reverse_string:
+reverse_string:
     push ebp
     mov ebp, esp
     push ebx                ; preserve ebx as required by cdecl
 
-    ; TODO: call the reverse_string() function and print the reversed string
+    mov eax, [ebp + 8]
+    mov ecx, [ebp + 12]
+    add eax, ecx
+    dec eax
+    mov edx, [ebp + 16]
 
-    pop ebx
+copy_one_byte:
+    mov bl, [eax]
+    mov [edx], bl
+    dec eax
+    inc edx
+    loopnz copy_one_byte
+
+    inc edx
+    mov byte [edx], 0
+
+    pop ebx                 ; restore ebx
     leave
     ret
+
+print_reverse_string:
+	push ebp
+	mov ebp, esp
+	push ebx                ; preserve ebx as required by cdecl
+
+
+	push store_string
+	push dword[ebp+12]
+	push dword[ebp+8]
+	call reverse_string
+	add esp, 12
+	
+	push store_string
+	call puts
+	add esp, 4
+
+	pop ebx
+	leave
+	ret
